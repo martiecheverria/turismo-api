2aa1b089e5b27395efe6fcff7e27386a52532228
martiecheverria <martiecheverria2005@gmail.com>
2025-12-12 13:48:55 -0300

    Update api-router.php

--- turismo-api/api-router.php
+++ turismo-api/api-router.php
@@ -1,23 +1,33 @@
 <?php
 require_once 'libs/router.php';
+
 require_once 'controllers/DestinoApiController.php';
-require_once 'controllers/RegionApiController.php';
+require_once 'controllers/AuthApiController.php';
+
+require_once 'middlewares/jwt.middleware.php';
+require_once 'middlewares/guard.middleware.php';
 
 $router = new Router();
 
+$router->addMiddleware(new JWTMiddleware());
 
-$router->addRoute('destinos',     'GET',    'DestinoApiController', 'getAll');
+//RUTAS PÚBLICAS
 
-$router->addRoute('destinos/:ID', 'GET',    'DestinoApiController', 'getDestino');
+$router->addRoute('auth/token',   'GET', 'AuthApiController',   'getToken');
+
+$router->addRoute('destinos',     'GET', 'DestinoApiController', 'getAll');
+$router->addRoute('destinos/:ID', 'GET', 'DestinoApiController', 'getDestino');
+
+
+$router->addMiddleware(new GuardMiddleware());
+
+
+//RUTAS PROTEGIDAS
 
 $router->addRoute('destinos',     'POST',   'DestinoApiController', 'createDestino');
-
 $router->addRoute('destinos/:ID', 'PUT',    'DestinoApiController', 'updateDestino');
-
 $router->addRoute('destinos/:ID', 'DELETE', 'DestinoApiController', 'deleteDestino');
 
 
-
-$resource = $_GET['resource'];
-$method = $_SERVER['REQUEST_METHOD'];
-$router->route($resource, $method);
+// Ejecución
+$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
